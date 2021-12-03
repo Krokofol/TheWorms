c72cc557440334c3ccd8f91121233cc3d360c08e
Aleksey <lakhanskii@skbkontur.ru>
2021-12-03 21:47:05 +0700

    Intellectual test

---
+++ TheWorms_CS_lab_Windows_Test/src/IntellectualTest.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using TheWorms_CS_lab_Windows.environment;
+using TheWorms_CS_lab_Windows.environment.objects;
+using TheWorms_CS_lab_Windows.services;
+
+namespace TheWorms_CS_lab_Windows_Test
+{
+    public class IntellectualTest
+    {
+        private readonly IntellectualService _intellectualService;
+        private readonly NameService _nameService;
+        private readonly LandSpace _landSpace;
+
+        public IntellectualTest()
+        {
+            FoodService foodService = new NullFoodService();
+            DirectionService directionService = new DirectionService();
+            _intellectualService = new IntellectualService(directionService);
+            _nameService = new NameService();
+            _landSpace = new LandSpace(
+                foodService,
+                _nameService,
+                directionService,
+                _intellectualService
+            );
+        }
+        
+        [Test]
+        public void MoveUpForFoodTest()
+        {
+            _landSpace.Objects.Clear();
+            var worm = new Worm(0, 0, "Move-or-die", _nameService, _intellectualService, _landSpace);
+            var food = new Food(0, 2);
+            _landSpace.Objects.Add(worm);
+            _landSpace.Objects.Add(food);
+            _landSpace.Update(1);
+            Assert.AreEqual(0, worm.PosX);
+            Assert.AreEqual(1, worm.PosY);
+        }
+        
+        [Test]
+        public void MoveDownForFoodTest()
+        {
+            _landSpace.Objects.Clear();
+            var worm = new Worm(0, 0, "Move-or-die", _nameService, _intellectualService, _landSpace);
+            var food = new Food(0, -2);
+            _landSpace.Objects.Add(worm);
+            _landSpace.Objects.Add(food);
+            _landSpace.Update(1);
+            Assert.AreEqual(0, worm.PosX);
+            Assert.AreEqual(-1, worm.PosY);
+        }
+        
+        [Test]
+        public void MoveRightForFoodTest()
+        {
+            _landSpace.Objects.Clear();
+            var worm = new Worm(0, 0, "Move-or-die", _nameService, _intellectualService, _landSpace);
+            var food = new Food(2, 0);
+            _landSpace.Objects.Add(worm);
+            _landSpace.Objects.Add(food);
+            _landSpace.Update(1);
+            Assert.AreEqual(1, worm.PosX);
+            Assert.AreEqual(0, worm.PosY);
+        }
+        
+        [Test]
+        public void MoveLeftForFoodTest()
+        {
+            _landSpace.Objects.Clear();
+            var worm = new Worm(0, 0, "Move-or-die", _nameService, _intellectualService, _landSpace);
+            var food = new Food(-2, 0);
+            _landSpace.Objects.Add(worm);
+            _landSpace.Objects.Add(food);
+            _landSpace.Update(1);
+            Assert.AreEqual(-1, worm.PosX);
+            Assert.AreEqual(0, worm.PosY);
+        }
+
+        private class NullFoodService : FoodService
+        {
+            public override Food? CreateFood()
+            {
+                return null;
+            }
+        }
+    }
+}
